33289d64acbbd2ff31f5ede1bdc2425c4286602a
princessmumbi <princessmumbi001@gmail.com>
2024-03-25 22:43:06 +0300

    array-filter-lab-complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const drivers =["Bobby","Sammy","Sally","Annette","Sarah","Bobby"]; 
+function findMatching (drivers,name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+ function fuzzyMatch (drivers,firstLetters){
+    return drivers.filter(driver => driver.startsWith (firstLetters))
+
+ }
+ function matchName (drivers,name){
+    return drivers.filter(match => match.name === name)
+ }
